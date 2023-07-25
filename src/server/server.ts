f92280578ce70f40f26de29aaf9e13f7cadbbdfb
ianmacdougald <ian@tartanltd.com>
2023-07-25 16:14:54 -0400

    Remove all database functionality from codebase

--- src/server/server.ts
+++ src/server/server.ts
@@ -6,32 +6,13 @@ import express, {
 } from 'express';
 import { fibController } from './controllers/fibController';
 import { ServerError } from '../types';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import { benchmarkController } from './controllers/benchmarkController';
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-
-// const mongoURI: string = 'THIS IS A MONGO URI TO BE IMPORTED FROM .ENV FILE'
-
-const mongoURI: string = process.env.MONGO_URI
-
-const db = mongoose.connection
-
-async function main() {
-  await mongoose.connect(mongoURI);
-}
-
-main().catch((err) => console.log(err));
-
-db.on('error', console.error.bind(console, 'connection error: '))
-db.once('open', () => {
-  console.log('Connected to database successfully')
-})
 
 app.get(
   '/api/fib-js/:value',
@@ -48,23 +29,6 @@ app.get(
     return res.status(200).json(res.locals);
   }
 );
-
-app.get(
-  '/api/fib/benchmark/:language',
-  benchmarkController.getBenchmarks,
-  (req: Request, res: Response) => {
-    return res.status(200).json(res.locals.result)
-  }
-)
-
-app.post(
-  '/api/fib/benchmark',
-  benchmarkController.postBenchmark,
-  (req: Request, res: Response) => {
-    return res.status(200).json(res.locals)
-  }
-)
-
 
 app.use('/', (req: Request, res: Response) => {
   return res.status(404).json('Error: page not found');
